e2af1854440f07b8bd85f1778cda3dbed5692a10
SamLizzie30 <toinga.samantha@gmail.com>
2024-10-03 15:35:09 +0200

    separate in models and utils the db and the console print

---
+++ models/blog.js
@@ -0,0 +1,27 @@
+const logger = require('../utils/logger')
+const mongoose = require('mongoose')
+
+const url = process.env.MONGODB_URI
+
+mongoose.connect(url).then(_ => {
+    logger.info("Connect to Mongo")
+}).catch(error => {
+    logger.error("Error connecting to MongoDB: " + error)
+})
+
+const blogSchema = mongoose.Schema({
+    title: String,
+    author: String,
+    url: String,
+    likes: Number
+})
+
+blogSchema.set('toJSON', {transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString(),
+    delete returnedObject._id
+    delete returnedObject.__v
+}})
+
+const Blog = mongoose.model("Blog", blogSchema)
+
+module.exports = Blog;
