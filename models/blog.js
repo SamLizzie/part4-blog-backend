0b4391b7cd25e2bc4e9ae2e3997f9b91aebaef6c
SamLizzie30 <toinga.samantha@gmail.com>
2024-10-03 15:56:24 +0200

    refactor app and index file

--- models/blog.js
+++ models/blog.js
@@ -1,12 +1,4 @@
-const logger = require('../utils/logger')
-const config = require('../utils/config')
 const mongoose = require('mongoose')
-
-mongoose.connect(config.MONGODB_URI).then(_ => {
-    logger.info("Connect to Mongo")
-}).catch(error => {
-    logger.error("Error connecting to MongoDB: " + error)
-})
 
 const blogSchema = mongoose.Schema({
     title: String,
